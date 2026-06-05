f4953029c5f26f2391e0f921bb980b7e76872711
beriapriliansya <beriapriliansya04@gmail.com>
2026-06-05 23:23:06 +0700

    last update

--- resources/views/admin/destinasi/edit.blade.php
+++ resources/views/admin/destinasi/edit.blade.php
@@ -1,9 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Edit Destinasi') }}
-        </h2>
-    </x-slot>
 
     <div class="container py-5">
         <div class="row justify-content-center">
--- resources/views/admin/destinasi/show.blade.php
+++ resources/views/admin/destinasi/show.blade.php
@@ -1,9 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Detail: ') }} {{ $destinasi->nama }}
-        </h2>
-    </x-slot>
 
     <!-- Breadcrumb -->
     <div class="container mt-4">
