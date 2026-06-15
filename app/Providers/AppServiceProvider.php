676cb5a9d7f4d2d3da4dc7e53766cab0933f1964
beriapriliansya <beriapriliansya04@gmail.com>
2026-06-15 15:18:14 +0700

    Configure and compile assets for production hosting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         try {
             if (!\Illuminate\Support\Facades\Schema::hasTable('fasilitas')) {
                 \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
